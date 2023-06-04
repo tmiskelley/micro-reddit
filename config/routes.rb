ad5430c73f1c396ea90e9f83bfffa748da8b9c68
Timothy Miskelley <miskelleytimothy@gmail.com>
2023-06-04 14:41:22 -0400

    Added Comment functionality to view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
+  resources :comments
   resources :posts
   root to: "posts#index"
 end
